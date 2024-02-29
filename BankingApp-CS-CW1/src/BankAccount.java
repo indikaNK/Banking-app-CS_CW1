7e51cc4af92891018de75d63c0e1e32948c10bbb
indikank <indikank95@gmail.com>
2024-02-29 08:42:35 +0530

    Commit:02 Added Method to deposit money. added synchronization to monitor class

--- BankingApp-CS-CW1/src/BankAccount.java
+++ BankingApp-CS-CW1/src/BankAccount.java
@@ -1,6 +1,9 @@
+import java.math.BigDecimal;
+
 public class BankAccount {
 
     // bank account class is the acting monitor class
+    //which means it does not to anything on its own
     /*
     * It consist of 2 main properties and 3 operations.
     * properties
@@ -12,20 +15,19 @@ public class BankAccount {
     * */
     private String accountNumber;
 
-    //this var may get changed frequently
-    private volatile double balance;
+    //this var may get changed frequently, so we add it as volatile
+    private volatile BigDecimal balance;
 
     //constructor
-
     public BankAccount(String accountNumber, double balance) {
-        super();
+
+        super(); //super kw is added automatically
         this.accountNumber = accountNumber;
-        this.balance = balance;
+        this.balance = new BigDecimal(balance);
     }
 
+    //methods that are working with a critical section are added as sync methods using synchronized keyword
     //getters - setters
-
-
     public String getAccountNumber() {
         return accountNumber;
     }
@@ -34,11 +36,25 @@ public class BankAccount {
         this.accountNumber = accountNumber;
     }
 
-    public double getBalance() {
+    public synchronized BigDecimal getBalance() {
         return balance;
     }
 
-    public void setBalance(double balance) {
-        this.balance = balance;
+    public synchronized void setBalance(double balance) {
+        this.balance = BigDecimal.valueOf(balance);
+    }
+
+    // these are the basic functionalities that are supported by my banking app
+
+    public synchronized void depositMoney(BigDecimal amount){
+        //if the amount is greater than 0 only, then we allow the transaction
+        if(amount.intValue() > 0 ){
+            //we should update the balance
+            BigDecimal amountTobeAdded = new BigDecimal(amount.intValue());
+            balance = balance.add(amountTobeAdded);
+
+            System.out.println(Thread.currentThread().getName() +", After Deposit Account Balance "+this.getBalance());
+
+        }
     }
 }
